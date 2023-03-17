72e7e0b6509c33bc2e51ebac89e3ed113b37ec29
Zeynep Deniz <zeynep351961@gmail.com>
2023-03-17 16:00:59 +0300

    .

--- src/ClientApp.jsx
+++ src/ClientApp.jsx
@@ -2,8 +2,10 @@ import { hydrateRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App"
 
-hydrateRoot(document.getElementById("root"),
-    <BrowserRouter>
-    <App/>
-    </BrowserRouter>
-)
+hydrateRoot(
+  document.getElementById("root"),
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
--- src/app.jsx
+++ src/app.jsx
@@ -1,17 +1,16 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useState, lazy, Suspense } from "react";
+import { useState } from "react";
 import AdoptedPetContext from "./AdoptedPetContext";
-
-const Details = lazy(() => import("./Details"));
-const SearchParams = lazy(() => import("./SearchParams"));
+import Details from "./Details";
+import SearchParams from "./SearchParams";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: Infinity,
       cacheTime: Infinity,
-      suspense: true,
     },
   },
 });
@@ -20,15 +19,9 @@ const App = () => {
   const adoptedPet = useState(null);
   return (
     <div>
-      <AdoptedPetContext.Provider value={adoptedPet}>
-        <QueryClientProvider client={queryClient}>
-          <Suspense
-            fallback={
-              <div className="loading-pane">
-                <h2 className="loader">🌀</h2>
-              </div>
-            }
-          >
+      <BrowserRouter>
+        <AdoptedPetContext.Provider value={adoptedPet}>
+          <QueryClientProvider client={queryClient}>
             <header>
               <Link to="/">Adopt Me!</Link>
             </header>
@@ -36,11 +29,13 @@ const App = () => {
               <Route path="/details/:id" element={<Details />} />
               <Route path="/" element={<SearchParams />} />
             </Routes>
-          </Suspense>
-        </QueryClientProvider>
-      </AdoptedPetContext.Provider>
+          </QueryClientProvider>
+        </AdoptedPetContext.Provider>
+      </BrowserRouter>
     </div>
   );
 };
 
-export default App;
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
