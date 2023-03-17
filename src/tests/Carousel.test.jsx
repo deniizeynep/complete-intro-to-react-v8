cd2e40f2e76c4a76cd8f7fdd715c45b63092a509
Zeynep Deniz <zeynep351961@gmail.com>
2023-03-17 15:34:28 +0300

    testing

--- src/tests/Carousel.test.jsx
+++ src/tests/Carousel.test.jsx
@@ -18,4 +18,4 @@ test("lets users click on thumbnails to make them the hero", async () => {
     expect(hero.src).toContain(image);
     expect(Array.from(thumb.classList)).toContain("active");
   }
-});
+})
